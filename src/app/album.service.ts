fe4b9172ef4865b3215f9864ccd60fedcda4d124
eric nicolas and verna santos <eric+verna@>
2018-06-11 16:38:55 -0700

    album with fb

---
+++ src/app/album.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Album } from './album.model';
+import { ALBUMS } from './mockalbums';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Injectable()
+export class AlbumService {
+  albums: FirebaseListObservable<any[]>;
+  constructor(private database: AngularFireDatabase) { 
+    this.albums = database.list('albums');
+  }
+  getAlbums() {
+    return this.albums;
+  }
+  getAlbumById(albumId: number){
+    for (var i = 0; i <= ALBUMS.length - 1; i++) {
+      if (ALBUMS[i].id === albumId) {
+        return ALBUMS[i];
+      }
+    }
+  }
+}
